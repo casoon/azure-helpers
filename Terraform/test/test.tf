daa6297181f2b148a45e340040fe863ec0109f73
casoon <joern.seidel@casoon.de>
2020-03-09 13:56:48 +0100

    Terraform

---
+++ Terraform/test/test.tf
@@ -0,0 +1,23 @@
+provider "azurerm" {
+    version = "=2.0.0"
+    subscription_id = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    client_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    client_secret   = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    tenant_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+    features {}
+}
+
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "bitte-loesch-mich"
+  location = "Germany West Central"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
